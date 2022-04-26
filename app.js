a633bf7606bbec612508d2d4b7d5251c229902b7
Pratik Chavan <pratikchavan@Pratiks-MacBook-Air.local>
2022-04-26 22:29:06 +0530

    Section 3: 39. wrap up

--- app.js
+++ app.js
@@ -1,46 +1,7 @@
 const http = require("http");
-const fs = require('fs');
 
-const server = http.createServer((req, res) => {
-  const { url, method, headers } = req;
+const requestHandler = require('./routes');
 
-  // console.log("\n-------------\n", url, method, headers, "\n-------------\n")
-
-  if (url === "/" || url === "/home") {
-      res.setHeader("content-type", 'text/html');
-      res.write("<html>");
-      res.write("<head><title>Home</title></head>");
-      res.write(`<body>
-        <h1>Home Page</h1> 
-        <hr /> 
-        <form method='post' action='/message'>
-            <label>messsage :<input type='text' name='massage' /></label>
-            <button type='submit'>Submit</button>
-        </form>
-      </body>`);
-      res.write("</html>");
-      return res.end();
-    }
-  else if (url === "/message" && method==='POST'){
-    // console.log('req', req)
-    const body = [];
-    req.on('data', (chunk) => {
-      body.push(chunk);
-    });
-
-    return req.on('end', () => {
-      const parsedBody = (Buffer.concat(body).toString().split('='))[1];
-      fs.writeFile('./message.txt', `\n${parsedBody}`, {
-        flag: 'a'
-      }, (_err) => {
-        res.statusCode = 302;
-        res.setHeader('location', '/');
-        return res.end();
-      });
-    });
-}
-  else if (url === "/about") return res.end("About Page");
-  else return res.end("Server Response");
-});
+const server = http.createServer(requestHandler);
 
 server.listen(1795);
