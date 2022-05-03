bdaf814c403be3dd85a9dc17f3e0bc9b2a6d8ceb
Pratik Chavan <pratikchavan@Pratiks-MacBook-Air.local>
2022-05-03 23:55:42 +0530

    S5: PROGRESS: V63: Handling Different Routes

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
-const http = require("http");
+const express = require('express');
 
-const server = http.createServer();
+const app = express();
 
-server.listen(1795);
+app.use('/add-product', (req, res, next) => {
+    console.log('In another middleware');
+    res.send('<h1>The "Add Product" page!</h1>');
+})
+
+app.use('/', (req, res, next) => {
+    console.log('In another middleware');
+    res.send('<h1>Hello From Express!</h1>');
+})
+
+app.listen(1795);
