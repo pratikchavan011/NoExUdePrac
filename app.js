648c49aa428f71abf5afe9ca109e0c47f258803f
Pratik Chavan <pratikchavan@Pratiks-MacBook-Air.local>
2022-05-03 23:33:57 +0530

    S5: INIT

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 const http = require("http");
 
-const requestHandler = require('./routes');
-
-const server = http.createServer(requestHandler);
+const server = http.createServer();
 
 server.listen(1795);
